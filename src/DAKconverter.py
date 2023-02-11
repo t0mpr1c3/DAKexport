26cfaffbc0fc882a79ba5f8015927047701e43d5
Tom Price <t0mpr1c3@gmail.com>
2023-02-11 18:15:07 -0500

    update DAKexporter module

--- src/DAKconverter.py
+++ src/DAKconverter.py
@@ -552,10 +552,10 @@ class DAKPatternConverter:
         header[0:3] = self.stp_magic
         self.height = im.height
         self.width = im.width
-        putWordAt(header, 0x03, self.height)
-        putWordAt(header, 0x05, self.width)
-        putWordAt(header, 0x07, self.height)
-        putWordAt(header, 0x09, self.width)
+        putWordAt(header, 0x03, self.width)
+        putWordAt(header, 0x05, self.height)
+        putWordAt(header, 0x07, self.width)
+        putWordAt(header, 0x09, self.height)
         putWordAt(header, 0x0B, x_repeats)
         putWordAt(header, 0x0D, y_repeats)
         header[0x2C] = 0x00 ## machine Fair Isle (max 2 colors per row)
